05eb0b4016463c8d0b32d6f599ee7d0e532e091e
madnessTroy <huutoanchu@gmail.com>
2021-11-20 11:46:44 +0700

    Add files for ASM1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import { STAFFS, DEPARTMENTS } from "./shared/staffs";
+import StaffListComponent from "./components/StaffListComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            staffs: STAFFS,
+            departments: DEPARTMENTS
+        }
+    };
+    render () {
+        return (
+            <div className="container-fluid">
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand className="p-3 text-light">
+                            TRANG QUẢN LÍ NHÂN VIÊN
+                        </NavbarBrand>
+                    </div>
+                </Navbar>
+                <StaffListComponent staffs = {this.state.departments && this.state.staffs} />
+            </div>
+        );
+    };
 }
 
 export default App;
---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"; //bootstrap
+import { Card, CardTitle, CardText } from "reactstrap"; // reactstrap
+import dateFormat from "dateformat"; //dateformat
+
+class StaffListComponent extends React.Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            selectedStaff: null
+        }
+    }
+
+    onStaffSelected(staff) {
+        this.setState({selectedStaff: staff})
+    }
+
+    renderStaffInfo(staff) {
+        if (staff != null) {
+            return (
+                <div>
+                    <Card className= "p-3 m-2">
+                        <CardTitle>Họ và tên: {staff.name}</CardTitle>
+                        <CardText>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
+                        <CardText>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
+                        <CardText>Phòng ban: {staff.department[this.props.departments.name]}</CardText>
+                        <CardText>Số ngày nghỉ còn: {staff.annualLeave}</CardText>
+                        <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
+                    </Card>
+                </div>
+            )
+        } else {
+            return (
+                <div className="pt-3">
+                    <h3>Hãy chọn 1 nhân viên để xem</h3>
+                </div>
+            )
+        }
+    }
+
+    render () {
+        const staffList = this.props.staffs.map((staff) => {
+            return (
+                <div key = "{staff.id}" className="mt-2 col-lg-4 col-md-6">
+                    <Card onClick = {() => this.onStaffSelected(staff)}>
+                        <CardTitle className="text-center">{staff.name}</CardTitle>
+                    </Card>
+                </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                <div className="row pt-4">
+                    { staffList }
+                </div>
+                <div className="row mt-5">
+                    { this.renderStaffInfo(this.state.selectedStaff) }
+                </div>
+            </div>
+        );
+    }
+}
+
+export default StaffListComponent
