c11aa006f7b9f08104d907997d50aafad276f512
madnessTroy <huutoanchu@gmail.com>
2021-12-02 18:12:26 +0700

    Add files for LAB07_01

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Main from './components/MainComponent';
+import Main from "./components/MainComponent";
 
-import './App.css';
+import "./App.css";
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
 
+const store = ConfigureStore();
+class App extends Component {
 
+  render() {
+    return (
+      <Provider store={ store } >
 
-class App extends Component{
+        <BrowserRouter>
+            <div>
+            <Main />
+            </div>
+        </BrowserRouter>
 
-
-  render(){
-
-    return(
-      <BrowserRouter>
-        <div>
-          <Main />
-        </div>
-      </BrowserRouter>
+      </Provider>
     );
-
   }
-
 }
 
 export default App;
