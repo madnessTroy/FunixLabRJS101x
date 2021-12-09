37b8e8f4b8665015132af236275faedd7e12727a
madnessTroy <huutoanchu@gmail.com>
2021-12-09 13:50:53 +0700

    Fixed action for staffs.js

--- src/redux/staffs.js
+++ src/redux/staffs.js
@@ -1,7 +1,15 @@
 import { STAFFS } from "../shared/staffs";
+import * as ActionTypes from "./ActionTypes"
 
 export const Staffs = (state = STAFFS, action) => {
     switch(action.type) {
+        case ActionTypes.ADD_STAFF:
+            var staff = action.payload
+            staff.id = state.length
+            staff.image = "/assets/images/alberto.png"
+
+            return state.concat(staff)
+
         default:
             return state
     }
