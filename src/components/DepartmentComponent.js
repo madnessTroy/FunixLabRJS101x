ce882e101637e52a4e39bfe8f42d60c3d6d782aa
madnessTroy <huutoanchu@gmail.com>
2021-12-28 19:39:48 +0700

    fix Department Components

--- src/components/DepartmentComponent.js
+++ src/components/DepartmentComponent.js
@@ -1,56 +1,43 @@
 import React from "react";
 
-import { Card, CardTitle } from "reactstrap";
-import { Link } from "react-router-dom";
-
-import { Loading } from "./LoadingComponent";
+import { Card, CardTitle, CardText } from "reactstrap";
 
 function RenderDepartment({ department }) {
 	return (
-		<Card className="m-2">
-			<Link to={`/departments/${department.id}`}>
-				<CardTitle className="p-2">
-					<h2>{department.name}</h2>
-				</CardTitle>
-			</Link>
-			<p className="p-3">
+		<Card>
+			<CardTitle className="p-2">
+				<h2>{department.name}</h2>
+			</CardTitle>
+			<CardText className="p-3">
 				<span>Số lượng nhân viên: {department.numberOfStaff}</span>
-			</p>
+			</CardText>
 		</Card>
 	);
 }
 
-class Department extends React.Component {
-	render() {
-		if (this.props.departmentsLoading) {
-			return <Loading />;
-			// Handle loading fail
-		} else if (this.props.departmentsErrMsg) {
-			return <h3>{this.props.departmentsErrMsg}</h3>;
-		}
-
-		const DepartmentList = this.props.departments.map((department) => {
-			return (
-				<div key={department.id} className="col-lg-4 col-md-6 g-5">
-					<RenderDepartment department={department} />
-				</div>
-			);
-		});
-
+function Department(props) {
+	const departmentList = props.departments.map((department) => {
 		return (
-			<React.Fragment>
-				<div className="containter-fluid">
-					<div className="row p-3">
-						<h3>PHÒNG BAN</h3>
-					</div>
-				</div>
-				<hr />
-				<div className="container">
-					<div className="row">{DepartmentList}</div>
-				</div>
-			</React.Fragment>
+			<div key={department.id} className="col-lg-4 col-md-6 g-5">
+				<RenderDepartment department={department} />
+			</div>
 		);
-	}
+	});
+
+	return (
+		<React.Fragment>
+			<div className="containter-fluid">
+				<div className="row p-3">
+					<h3>PHÒNG BAN</h3>
+					<hr />
+				</div>
+			</div>
+
+			<div className="container">
+				<div className="row">{departmentList}</div>
+			</div>
+		</React.Fragment>
+	);
 }
 
 export default Department;
